80874766c82855b5b103866a7beaa1fd28eedcf6
Jay Pandya <38314089+mrjaypandya@users.noreply.github.com>
2018-12-27 16:47:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ server.post('/get-details', (req, res) => {
     });
 });
 
-server.listen((process.env.PORT || 8000), () => {
+server.listen((process.env.PORT || 8001), () => {
     console.log("Server is up and running...");
 });
